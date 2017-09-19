bbcc43908d27f41efe85a179b8d1e1a676204bc4
MatheusSSouza <matheussilvadsouza@gmail.com>
2017-09-19 18:27:36 -0300

    array para escolha de imagem

--- app/src/main/java/project/proximitychat/chat_list.java
+++ app/src/main/java/project/proximitychat/chat_list.java
@@ -1,24 +1,50 @@
 package project.proximitychat;
 
+
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class chat_list extends ListActivity {
 
+public class chat_list extends Activity{
+
+    ListView list;
     String[] itemname ={
-        "Jardim Miranda - Grupo do Bairo",
-        "Pokemon GO",
-        "Grupo da Materia Dificil",
+            "Jardim Miranda - Grupo do Bairo",
+            "Pokemon GO",
+            "Grupo da Materia Dificil",
     };
 
+    Integer[] imgid={
+            R.drawable.bairro,
+            R.drawable.pokemongo,
+            R.drawable.estudo,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_list);
+        setContentView(R.layout.content_chat_list);
 
-        this.setListAdapter(new ArrayAdapter<String>(
-                this, R.layout.chat_list,
-                R.id.Itemname,itemname));
+        ChatListAdapter adapter=new ChatListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
